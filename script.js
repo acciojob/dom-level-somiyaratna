34e60aed8d0c4c80c7f1a7de3279169d2cfecabf
Somiya Ratna Behera <beherasomiyaratna@gmail.com>
2023-10-04 21:19:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let levelElement = document.getElementById("level");
+let level = 1;
+
+while (true) {
+	if(levelElement.tagName === "HTML") {
+		break;
+	}
+	levelElement= levelElement.parentNode;
+	level++;
+}
+alert(`The level of the element is: ${level}`);
